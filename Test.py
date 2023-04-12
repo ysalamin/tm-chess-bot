9b18d5a421e9df7bffa2b318583a8ac85f8713b7
Blaazen <blqzen@gmail.com>
2023-04-12 19:14:03 +0200

    c'est bon maintenant ? ça c'étais pas update

--- Test.py
+++ Test.py
@@ -1,57 +1,33 @@
-# Example file showing a circle moving on screen
 import pygame
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
+# Variables
+WIDTH, HEIGHT = 1280, 720
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+def initialisation(WIDTH, HEIGHT):
+    # Initialisation de l'interface graphique
+    pygame.init()
+    # Création de la fenêtre avec les dimensions données
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    #Titre
+    pygame.display.set_caption("Chess board")
+    # Création d'un objet qui est utile pour gérer le temps
+    clock = pygame.time.Clock()
+    #On crée la variable qui start la partie
+    running = True
 
-while running:
+def chess_board():
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def chess_pieces():
 
-    screen.fill("blue")
+def main():
+    #Boucle de jeu
+    initialisation(WIDTH, HEIGHT)
+    while running :
+        for event in pygame.event.get():
+            # Si la croix est cliquée, quitte la boucle -> pygame.quit() sera lu
+            if event.type == pygame.QUIT:
+                running = False
 
-    pygame.draw.circle(screen, "red", player_pos, 5)
+    #Quitte tout
+    pygame.quit()
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(30) / 1000
-
-pygame.quit()
-
-
-
-
-
-
-Board = [
-    [-5.63, -3.33, -3.05, -1000, -9.5, -3.05, -3.33, -5.63],
-    [-1, -1, -1, -1, -1, -1, -1, -1],
-    [],
-    [],
-    [],
-    [],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [5.63, 3.33, 3.05, 1000, 9.5, 3.05, 3.33, 5.63],
-]
